870f44d4ec9b7898998fe437c33744fe160ce1db
mei <meike.pollakowski@gmail.com>
2021-04-16 11:33:47 +0200

    Improve game over page

--- lib/tetris_web/live/game_live/game_over.ex
+++ lib/tetris_web/live/game_live/game_over.ex
@@ -1,9 +1,16 @@
 defmodule TetrisWeb.GameLive.GameOver do
   use TetrisWeb, :live_view
-  alias Tetris.Game
 
-  def mount(_params, _session, socket) do
+  def mount(params, _session, socket) do
     {:ok, 
-    assign(socket, game: Map.get(socket.assigns, :game) || Game.new)}
+    assign(socket, score: params["score"] || 0)}
+  end
+
+  defp play(socket) do
+    push_redirect(socket, to: "/game/playing")
+  end
+
+  def handle_event("play", _, socket) do
+    {:noreply, play(socket)}
   end
 end
