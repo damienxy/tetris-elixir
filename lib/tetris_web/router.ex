7c38d8d18be181c784b882938fe2d96a35c90395
mei <meike.pollakowski@gmail.com>
2021-05-24 13:53:27 +0200

    Bring back dashboard route

--- lib/tetris_web/router.ex
+++ lib/tetris_web/router.ex
@@ -18,6 +18,21 @@ defmodule TetrisWeb.Router do
     pipe_through :browser
 
     live "/", GameLive, :index
+
+      # Enables LiveDashboard only for development
+      #
+      # If you want to use the LiveDashboard in production, you should put
+      # it behind authentication and allow only admins to access it.
+      # If your application does not have an admins-only section yet,
+      # you can use Plug.BasicAuth to set up some basic authentication
+      # as long as you are also using SSL (which you should anyway).
+      if Mix.env() in [:dev, :test] do
+        import Phoenix.LiveDashboard.Router
+
+        live_dashboard "/dashboard", metrics: TetrisWeb.Telemetry
+      end
+
+    # Serve GameLive for any route
     live "/*path", GameLive, :index
   end
 
@@ -25,20 +40,4 @@ defmodule TetrisWeb.Router do
   # scope "/api", TetrisWeb do
   #   pipe_through :api
   # end
-
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
-
-    scope "/" do
-      pipe_through :browser
-      live_dashboard "/dashboard", metrics: TetrisWeb.Telemetry
-    end
-  end
 end
